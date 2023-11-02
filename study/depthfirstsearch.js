9a2f200834b34aad04f9ce27fd323af85a627e42
kevinwonart <kevin.won91@gmail.com>
2023-11-01 22:19:33 -0500

    studying DFS/BFS before pursuing minimax algo for tictactoe. switching to JS instead python also

---
+++ study/depthfirstsearch.js
@@ -0,0 +1,83 @@
+/*ChatGPT convo
+what is depth in algorithm? what is the first algorithm to learn that introduces the depth concept. Im trying to learn how minimax algo works but maybe there's a preliminary algo to it
+ChatGPT
+
+In algorithms and computer science, "depth" often refers to the depth of a tree or graph structure. It's commonly used in various algorithms and concepts, including search algorithms, tree traversal, and graph analysis. The depth of a node in a tree or graph is a measure of how many steps or levels it is away from the root node.
+
+The concept of depth is often introduced in the context of tree data structures and algorithms that work with trees. A common algorithm that introduces the depth concept is the Depth-First Search (DFS) algorithm. DFS is used to traverse trees and graphs and can be implemented using both recursive and non-recursive approaches.
+
+Here's a brief overview of the Depth-First Search algorithm:
+
+    In DFS, you start at the root node of a tree or graph and explore as far as possible along each branch before backtracking.
+    During the traversal, you can keep track of the depth of each node, which is the level of the node in the tree or graph.
+    Depth is often used to determine the distance between nodes, find the depth of a node, or explore nodes in a specific order.
+
+Depth-First Search is a fundamental algorithm that forms the basis for many other algorithms, including the minimax algorithm. The minimax algorithm is used in game theory, particularly in games like chess and tic-tac-toe, to determine the best move to make by exploring game states and evaluating them at different depths.
+
+If you're interested in learning how the minimax algorithm works, it's a good idea to first get comfortable with concepts related to trees, graphs, and depth. Depth-First Search is a great starting point for understanding these concepts and can serve as a foundation for more advanced algorithms like minimax.
+User
+okay give me a coding problem that involves depth first search so i can practice it
+ChatGPT
+
+Sure, here's a coding problem that involves Depth-First Search (DFS). This problem asks you to implement a DFS algorithm to find the connected components in an undirected graph.
+
+Problem: Find Connected Components in an Undirected Graph
+
+You are given an undirected graph as an adjacency list. Write a function to find the connected components in the graph.
+
+Each connected component is a set of nodes where each node is connected to every other node in the set through some path.
+
+Your task is to write a function findConnectedComponents(graph) that takes an adjacency list representation of the graph and returns a list of connected components.
+
+For example:
+
+javascript
+
+const graph = {
+  0: [1, 2],
+  1: [0, 2],
+  2: [0, 1],
+  3: [4],
+  4: [3],
+  5: [6],
+  6: [5],
+};
+
+console.log(findConnectedComponents(graph));
+
+Output:
+
+lua
+
+[[0, 1, 2], [3, 4], [5, 6]]
+
+You can solve this problem using a depth-first search (DFS) algorithm to traverse the graph and find connected components. You can start at a node, explore its neighbors, and mark visited nodes. Continue this process until you've visited all nodes in a connected component, then move on to the next unvisited node.
+
+This problem will give you a good opportunity to practice implementing a basic DFS algorithm and understanding connected components in a graph.
+*/
+
+function findConnectedComponents(graph) {
+  let answer = [];
+  let traversedNode = new Set();
+  for(let node in graph){
+    if(!traversedNode.has(node)) {
+      traversedNode.add(node);
+    }
+    for(let adjNodes of graph[node]){
+
+    }
+  }
+
+}
+
+const graph = {
+  0: ["1", "2"],
+  1: ["0", "2"],
+  2: ["0", "1"],
+  3: ["4"],
+  4: ["3","7"],
+  5: ["6"],
+  6: ["5"],
+};
+findConnectedComponents(graph);
+//console.log(findConnectedComponents(graph));
