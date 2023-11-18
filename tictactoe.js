2f7537c43a823cdd3373babd2e9a6bf036f35b8f
Kevin Won <kevin.won91@gmail.com>
2023-11-18 11:51:29 -0600

    add statecheck and minmax need to add bot to play

--- tictactoe.js
+++ tictactoe.js
@@ -39,30 +39,128 @@ function switchPlayer() {
   turn = turn === "x" ? "o" : "x";
 }
 
-function checkWin(board, player){
-  for(let i = 0; i < 3; i++) {
-    if((board[i][0] === player &&
-      board[i][1] === player &&
-      board[i][2] === player) ||
-      board[0][i] === player &&
-      board[1][i] === player &&
-      board[2][i] === player
-    ){
+function isGameOver(board){
+  for (let i = 0; i < 3; i++) {
+    if(board[i][0] === "x" && board[i][0] === board[i][1] === board[i][2]){
+      return true;
+    }
+    if(board[0][i] === "x" && board[0][i] === board[1][i] === board[2][i]){
+      return true;
+    }
+    if(board[i][0] === "o" && board[i][0] === board[i][1] === board[i][2]){
+      return true;
+    }
+    if(board[0][i] === "o" && board[0][i] === board[1][i] === board[2][i]){
       return true;
     }
   }
-  if(board[0][0] === player &&
-    board[1][1] === player &&
-    board[2][2] === player) {
-      return true;
+  if(board[0][0] === "x" && board[0][0] === board[1][1] === board[2][2]){
+    return true;
   }
-  if(board[0][2] === player &&
-    board[1][1] === player &&
-    board[2][0] === player) {
-      return true;
+  if(board[0][2] === "x" && board[0][2] === board[1][1] === board [2][0]){
+    return true;
+  }
+  if(board[0][0] === "o" && board[0][0] === board[1][1] === board[2][2]){
+    return true;
+  }
+  if(board[0][2] === "o" && board[0][2] === board[1][1] === board [2][0]){
+    return true;
+  }
+  function isBoardFilled(board){
+    for(let row of board) {
+      for(let element of row) {
+        if(element === "") {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
 
+function evalBoard(board){
+  for(let i = 0; i < 3; i++) {
+    if((board[i][0] === "x" &&
+      board[i][1] === "x" &&
+      board[i][2] === "x") ||
+      board[0][i] === "x" &&
+      board[1][i] === "x" &&
+      board[2][i] === "x"
+    ){
+      return 1;
+    }
+  }
+  if(board[0][0] === "x" &&
+    board[1][1] === "x" &&
+    board[2][2] === "x") {
+      return 1;
+  }
+  if(board[0][2] === "x" &&
+    board[1][1] === "x" &&
+    board[2][0] === "x") {
+      return 1;
+  }
+
+  if((board[i][0] === "o" &&
+    board[i][1] === "o" &&
+    board[i][2] === "o") ||
+    board[0][i] === "o" &&
+    board[1][i] === "o" &&
+    board[2][i] === "o"
+  ){
+    return -1;
+  }
+  }
+  if(board[0][0] === "o" &&
+    board[1][1] === "o" &&
+    board[2][2] === "o") {
+      return -1;
+  }
+  if(board[0][2] === "o" &&
+    board[1][1] === "o" &&
+    board[2][0] === "o") {
+      return -1;
+  }
+  return 0;
+}
+
+function miniMax(board, depth, isMax) {
+  if(isGameOver(board){
+    return evalBoard(board);
+  }
+
+  if (isMax){
+    let maxEval = -Infinty;
+    for (let row = 0; row < 3; row++){
+      for (let col = 0; col < 3; col++){
+        if(board[row][col] === ""){
+          board[row][col] = "x";
+          const eval = miniMax(board, depth + 1; false);
+          board[row][col] = "";
+          maxEval = Math.max(maxEval, eval);
+        }
+      }
+    }
+    return maxEval;
+  } else {
+    let minEval = Infinity;
+    for (let row = 0; i < 3; row++) {
+      for (let col = 0; j < 3; col++) {
+        if (board[row][col] === ""){
+          board[row][col] = "o";
+          const eval = miniMax(board, depth + 1; true);
+          board[row][col] = "";
+          minEval = Math.min(minEval, eval);
+        }
+      }
+    }
+    return minEval;
+    }
+}
+
+function findMove(board, player){
+
+}
 
 function play(){
   printBoard();
