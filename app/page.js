bfac9cfb4666700f5237b67bcbd2cae1aeaaf404
Kevin Won <kevin.won91@gmail.com>
2023-12-15 16:28:33 -0600

    test gitignore added pages/board component

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from '..src/Board.js'
+
+export default function Page() {
+  return {
+    <div>
+      <Board />
+    </div>
+  };
+}
+
---
+++ src/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/Board.css';
+
+const Board = () => {
+  return (
+    <div className="board">
+    </div>
+  );
+};
+
+export default Board;
