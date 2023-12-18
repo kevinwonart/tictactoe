de74ecb2753968cef12156aea2b37fe68ac6c98f
Kevin Won <kevin.won91@gmail.com>
2023-12-18 17:53:57 -0600

    add buttons/tile

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,6 +4,15 @@ import styles from './Board.css';
 const Board = () => {
   return (
     <div className="board">
+      <button class="tile" id="tile1"></button>
+      <button class="tile" id="tile2"></button>
+      <button class="tile" id="tile3"></button>
+      <button class="tile" id="tile4"></button>
+      <button class="tile" id="tile5"></button>
+      <button class="tile" id="tile6"></button>
+      <button class="tile" id="tile7"></button>
+      <button class="tile" id="tile8"></button>
+      <button class="tile" id="tile9"></button>
     </div>
   );
 };
