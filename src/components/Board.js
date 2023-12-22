be21512cc1a5c7f0f628b0c9a3e77b55b10bffdd
Kevin Won <kevin.won91@gmail.com>
2023-12-21 19:09:35 -0600

    stuck on rendering limit

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,37 +1,44 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Board.css';
+import * as MiniMaxUtils from './minimax';
 
-const player1 = "/images/x.png";
-const player2 = "/images/o.png";
+const x = "/images/x.png";
+const o = "/images/o.png";
+const player1 = '/images/x.png';
+const player2 = '/images/o.png';
 
 const Board = () => {
   
   let move = player1;
+  const initialTiles = Array.from({ length: 3 }, () => new Array(3).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState(player1);
-  const [tiles, setTiles] = useState(Array(9).fill(null));
-
-  const handleClick = (index) => {
-    if(tiles[index] === null) {
-      const newTiles = [...tiles];
-      newTiles[index] = currentPlayer;
+  const [tiles, setTiles] = useState(initialTiles);
+  const [boardFilled, setBoardFilled] = useState(false);
+  const handleClick = (row, col) => {
+    if(tiles[row][col] === null) {
+      const newTiles = tiles.map(array => [...array]);
+      newTiles[row][col] = currentPlayer;
       setTiles(newTiles);
       setCurrentPlayer(currentPlayer === player1 ? player2 : player1);
     }
   };
-
+  
   return (
     <div className="board">
-      {tiles.map((onClickImage, index) => (
-        <button
-          key={index}
-          className="tile"
-          id={`tile${index}`}
-          onClick={() => handleClick(index)}
-        >
-          {onClickImage && <img src={onClickImage}/>}
-        </button>
+      {tiles.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((col, colIndex) => (
+            <button
+              key={rowIndex * tiles.length + colIndex}
+              className="tile"
+              onClick={() => handleClick(rowIndex, colIndex)}
+            >
+              {col && <img src={col} />}
+            </button>
+          ))}
+        </div>
       ))}
     </div>
   );
